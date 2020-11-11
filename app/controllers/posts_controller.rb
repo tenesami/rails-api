9cc88c159103305ec353eb3b802d98da4487f54f
Tesfaye Atomsa <tesfayeatomsa@gmail.com>
2020-11-10 22:09:53 -0700

    post controller permit username, content

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -46,6 +46,6 @@ class PostsController < ApplicationController
 
     # Only allow a trusted parameter "white list" through.
     def post_params
-      params.require(:post).permit(:title)
+      params.require(:post).permit(:title, :content, :username)
     end
 end
