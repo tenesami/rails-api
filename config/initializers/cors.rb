178eb1e3a85cd2cd6c9d466929877bc3c478d2fe
Tesfaye Atomsa <tesfayeatomsa@gmail.com>
2020-11-10 20:05:58 -0700

    go config initialzers cors uncomment and add request url

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -5,12 +5,15 @@
 
 # Read more: https://github.com/cyu/rack-cors
 
-# Rails.application.config.middleware.insert_before 0, Rack::Cors do
-#   allow do
-#     origins 'example.com'
-#
-#     resource '*',
-#       headers: :any,
-#       methods: [:get, :post, :put, :patch, :delete, :options, :head]
-#   end
-# end
+#########################
+#for security	reason it good idea to have CORS protection to specify 
+#and know from where the request is coming from and what type of request 
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    origins 'example.com'
+
+    resource '*',
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+  end
+end
