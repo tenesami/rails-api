bf300012687f8e9cbeefd55aeee22dc354424d2e
Tesfaye Atomsa <tesfayeatomsa@gmail.com>
2020-11-11 19:15:53 -0700

    add custom js error method in post contrller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -20,7 +20,7 @@ class PostsController < ApplicationController
     if @post.save
       render json: @post, status: :created, location: @post
     else
-      render json: @post.errors, status: :unprocessable_entity
+      render json: {errors: @post.errors.full_messages}, status: :unprocessable_entity
     end
   end
 
